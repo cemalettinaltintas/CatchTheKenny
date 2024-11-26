2d91249dbf8677042bc376f8cd36012b086994ea
cemalettinaltintas <cemalettinaltintas@gmail.com>
2024-11-26 14:46:08 +0300

    Max score added.

--- app/src/main/java/com/cemalettinaltintas/catchthekenny/MainActivity.java
+++ app/src/main/java/com/cemalettinaltintas/catchthekenny/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cemalettinaltintas.catchthekenny;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     TextView scoreText;
     TextView timeText;
     int score;
+    int maxScore;
     ImageView imageView1;
     ImageView imageView2;
     ImageView imageView3;
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView[] imageArray;
     Runnable runnable;
     Handler handler;
+    SharedPreferences sharedPreferences;
+    TextView maxScoreText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         //initalize
         scoreText=findViewById(R.id.scoreText);
         timeText=findViewById(R.id.timeText);
-
+        maxScoreText=findViewById(R.id.maxScoreText);
         imageView1=findViewById(R.id.imageView1);
         imageView2=findViewById(R.id.imageView2);
         imageView3=findViewById(R.id.imageView3);
@@ -61,12 +66,23 @@ public class MainActivity extends AppCompatActivity {
         imageArray=new ImageView[]{imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
         score=0;
 
+        sharedPreferences=this.getSharedPreferences("com.cemalettinaltintas.catchthekenny", Context.MODE_PRIVATE);
+        maxScore=sharedPreferences.getInt("userScore",0);
+        if (maxScore!=0){
+            maxScoreText.setText(String.valueOf(maxScore));
+        }
+
         hideImages();
 
         new CountDownTimer(10000, 1000) {
             @Override
             public void onTick(long l) {
                 timeText.setText("Time :"+l/1000);
+                if (maxScore<score){
+                    maxScore=score;
+                    maxScoreText.setText(String.valueOf(maxScore));
+                    sharedPreferences.edit().putInt("userScore",maxScore).apply();
+                }
             }
 
             @Override
@@ -76,28 +92,32 @@ public class MainActivity extends AppCompatActivity {
                 for (ImageView image:imageArray){
                     image.setVisibility(View.INVISIBLE);
                 }
-                AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
-                alert.setTitle("Restart?");
-                alert.setMessage("Are you sure to restart game?");
-                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //restart
-                        Intent intent=getIntent();
-                        finish();
-                        startActivity(intent);
-                    }
-                });
-                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this,"Game Over!",Toast.LENGTH_LONG).show();
-                    }
-                });
-
-                alert.show();
+                loadAlert();
             }
         }.start();
+    }
+
+    private void loadAlert() {
+        AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle("Restart?");
+        alert.setMessage("Are you sure to restart game?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //restart
+                Intent intent=getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Game Over!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
     }
 
     public void increaseScore(View view) {
@@ -116,10 +136,9 @@ public class MainActivity extends AppCompatActivity {
                 Random random=new Random();
                 int i= random.nextInt(9);
                 imageArray[i].setVisibility(View.VISIBLE);
-                handler.postDelayed(runnable,500);
+                handler.postDelayed(runnable,1000);
             }
         };
         handler.post(runnable);
-
     }
 }
